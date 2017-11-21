743c2e2aab497b6380161a046e4ab16d038b2dc8
Alejandro <alu0100973792@ull.edu.es>
2017-11-21 15:50:05 +0000

    creada prueba para el indice glucemico

--- spec/Alimento_spec.rb
+++ spec/Alimento_spec.rb
@@ -7,6 +7,15 @@ RSpec.describe Alimento do
 		@yogurt = Alimento::Alimento.new('Yogurt', 3.8, 4.9, 3.8)
 		@aceite = Alimento::Alimento.new('Aceite de oliva', 0.0, 0.2, 99.6)
 		@cebolla = Alimento::Alimento.new('Cebolla', 1.3, 5.8, 0.3)
+		
+		@datos_glucosa = [[4.9, 5.3, 5.9, 6.7, 7.2, 7.6, 8.0, 8.2, 8.2, 8.4, 8.3, 8.3, 8.0, 7.5, 7.1, 6.8, 6.8, 6.9, 6.8, 6.3, 6.2, 6.3, 6.2, 6.3, 6.1],
+						  [6.3, 5.4, 5.6, 5.7, 6.5, 7.4, 7.9, 7.4, 7.7, 7.9, 7.9, 7.8, 7.8, 7.8, 8.0, 8.5, 9.4, 10.8,10.5,9.1, 8.9,8.3,7.7,7.6,7.5]]
+		@datos_compota =  [[6.7, 6.5, 6.8, 6.9, 7.0, 7.1, 6.9, 6.9, 6.9, 6.7, 6.9, 7.3, 7.0, 7.0, 7.2, 7.1, 6.8, 7.2, 7.3, 7.0, 6.8, 6.7, 6.8, 6.7, 6.9],
+						  [4.6, 4.8, 5.3, 5.6, 6.1, 6.5, 6.6, 7.0, 7.0, 6.8, 6.4, 6.3, 6.1, 6.1, 6.2, 6.0, 6.1, 6.1, 6.2, 6.3, 6.4, 6.1, 6.1, 5.7, 5.9]]
+		@datos_yogurt = [[6.1, 6.6, 6.3, 6.3, 6.1, 6.9, 6.8, 6.5, 6.4, 6.9, 6.8, 6.5, 6.3, 6.2, 6.7, 6.2, 5.9, 5.8, 5.8, 5.8, 5.8, 5.8, 5.9, 6.2, 6.4],
+						 [4.9, 4.9, 5.2, 5.8, 6.5, 7.0, 7.2, 7.3, 7.3, 6.6, 6.2, 6.1, 6.0, 6.1, 5.9, 5.9, 5.9, 5.9, 5.8, 5.8, 5.5, 5.5, 5.6, 5.9, 5.9]]
+		@datos_chocolate = [[6.5, 6.5, 6.7, 6.5, 6.5, 6.8, 6.7, 6.2, 6.5, 7.2, 6.9, 7.0, 6.3, 6.2, 6.1, 5.9, 5.8, 6.1, 6.7, 6.7, 6.6, 6.7, 6.9, 7.2, 7.1],
+							[4.6, 4.6, 4.7, 4.7, 4.8, 4.7, 4.8, 4.8, 4.6, 4.4, 4.7, 4.7, 4.8, 4.7, 5.2, 5.9, 5.9, 5.7, 5.4, 5.3, 5.1, 4.8, 4.8, 4.9, 5.9]]
 	end
 
 	it "has a version number" do
@@ -41,6 +50,15 @@ RSpec.describe Alimento do
 	it "es comparable" do
 		expect(@lechevaca == @aceite).to eq(false)
 		expect(@lechevaca < @aceite).to eq(true)
+	end
+	
+	it "tiene un método para calcular el Índice Glucémico" do
+		# compota de manzana = 50.9?
+		expect(abs(Alimento.new("Compota de manzana", 1, 1, 1).indice_glucemico(@datos_compota, @datos_glucosa) - 50.9)).to be < 10
+		# yogurt = 47.3?
+		expect(abs(Alimento.new("Yogurt", 1, 1, 1).indice_glucemico(@datos_yogurt, @datos_glucosa) - 47.3)).to be < 10
+		# chocolate = 53.1?
+		expect(abs(Alimento.new("Chocolate", 1, 1, 1).indice_glucemico(@datos_chocolate, @datos_glucosa) - 53.1)).to be < 10
 	end
 end
 
