42c8f2992416fcf59013df2322fe2260ed22b354
Alejandro <alu0100973792@ull.edu.es>
2017-11-28 23:31:13 +0000

    coveralls añadido

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require "bundler/setup"
 require "Alimento"
 require "Alimento/List"
 require "Alimento/TiposAlimento"
+require 'coveralls'
+Coveralls.wear!
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
