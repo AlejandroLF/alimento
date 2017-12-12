fac0d13e26bb7d1cf93bc768a72163b5a99f4a4c
Alejandro <alu0100973792@ull.edu.es>
2017-12-12 19:43:33 +0000

    primeras pruebas dsl definidas

---
+++ spec/PlatoHarvard_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+RSpec.describe PlatoHarvard do
+	before :each do
+
+	end
+
+	it "esta definido" do
+		mac_and_cheese = PlatoHarvard.new("Nombre") {}
+	end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require "bundler/setup"
 require "Alimento"
 require "Alimento/List"
 require "Alimento/TiposAlimento"
+require "Alimento/PlatoHarvard"
 require 'coveralls'
 Coveralls.wear!
 
